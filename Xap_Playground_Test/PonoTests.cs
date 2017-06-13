0e8ecfaffa42e0c854a699cc31fc8eaf132bb43d
andreas mewald <andreas.mewald@pdts.at>
2017-06-13 11:45:24 +0200

    Added project

---
+++ Xap_Playground_Test/PonoTests.cs
@@ -0,0 +1,61 @@
+﻿using GigaSpaces.Core;
+using GigaSpaces.Core.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GigaSpaces.Core.Metadata;
+
+namespace Xap_Playground_Test
+{
+    [TestFixture]
+    public class PonoTests
+    {
+        public class Person
+        {
+            [SpaceID]
+            public int? Ssn { get; set; }
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+        }
+
+        [Test]
+        public void CreatePonoInSpace()
+        {
+            Console.WriteLine("Connecting to data grid...");
+            ISpaceProxy spaceProxy = new SpaceProxyFactory("myDataGrid").Create();
+
+            Console.WriteLine("Write (store) a couple of entries in the data grid:");
+            spaceProxy.Write(new Person { Ssn = 1, FirstName = "Vincent", LastName = "Chase" });
+            spaceProxy.Write(new Person { Ssn = 2, FirstName = "Johnny", LastName = "Drama" });
+
+            Console.WriteLine("Read (retrieve) an entry from the grid by its id:");
+            Person person1 = spaceProxy.ReadById<Person>(1);
+            person1.Should().NotBeNull();
+            person1.FirstName.Should().BeEquivalentTo("Vincent");
+            person1.LastName.Should().BeEquivalentTo("Chase");
+
+            Console.WriteLine("Result: " + person1);
+
+            Console.WriteLine("Read an entry from the grid using LINQ:");
+            var query = from p in spaceProxy.Query<Person>()
+                        where p.FirstName == "Johnny"
+                        select p;
+
+            var spaceQuery = query.ToSpaceQuery();
+
+            Person person2 = spaceProxy.Read<Person>(spaceQuery);
+            person2.LastName.Should().BeEquivalentTo("Drama");
+
+            Console.WriteLine("Result: " + person2);
+
+            Console.WriteLine("Read all entries of type Person from the grid:");
+            Person[] results = spaceProxy.ReadMultiple(new Person());
+            results.Count().Should().Be(2);
+            Console.WriteLine("Result: " + String.Join<Person>(",", results));
+        }
+    }
+}
---
+++ Xap_Playground_Test/SpaceDocumentTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NUnit.Framework;
+using GigaSpaces.Core;
+using GigaSpaces.Core.Document;
+using GigaSpaces.Core.Metadata;
+using FluentAssertions;
+
+namespace Xap_Playground_Test
+{
+    [TestFixture]
+    public class SpaceDocumentTests
+    {
+        private const string SpaceName = "myDataGrid";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [Description("Create an object using space document. Write that object to the space, read it afterwards and check some of the properties")]
+        public void CreateSpaceDocument ()
+        {
+            using (var spaceProxy = new SpaceProxyFactory(SpaceName).Create())
+            {
+                var document = CreateDocumemt();
+                RegisterProductTypeAt(spaceProxy);
+
+                spaceProxy.Write(document);
+
+                SpaceDocument template = new SpaceDocument("Product");
+                template["CatalogNumber"] = "av-9876";
+
+                var readDocumentFromSpace = spaceProxy.Read(template);
+
+                readDocumentFromSpace.Should().NotBeNull();
+                Assert.IsTrue(Equals(readDocumentFromSpace["Name"], "Jet Propelled Pogo Stick"));
+
+                var price = readDocumentFromSpace["Price"];
+                price.Should().Be(19.99);
+            }
+
+            // Local Helpers
+            SpaceDocument CreateDocumemt()
+            {
+                DocumentProperties properties = new DocumentProperties();
+
+                properties["CatalogNumber"] = "av-9876";
+                properties["Category"] = "Aviation";
+                properties["Name"] = "Jet Propelled Pogo Stick";
+                properties["Price"] = 19.99;
+                properties["Tags"] = new String[4] { "New", "Cool", "Pogo", "Jet" };
+
+                DocumentProperties p2 = new DocumentProperties();
+                p2["Manufacturer"] = "Acme";
+                p2["RequiresAssembly"] = true;
+                p2["NumberOfParts"] = 42;
+                properties["Features"] = p2;
+
+                SpaceDocument document = new SpaceDocument("Product", properties);
+
+                return document;
+            }
+
+            void RegisterProductTypeAt(ISpaceProxy proxy)
+            {
+                // Create type Document descriptor:
+                SpaceTypeDescriptorBuilder typeBuilder = new SpaceTypeDescriptorBuilder("Product");
+                typeBuilder.SetIdProperty("CatalogNumber");
+                typeBuilder.SetRoutingProperty("Category");
+                typeBuilder.AddPathIndex("Name");
+                typeBuilder.AddPathIndex("Price", SpaceIndexType.Extended);
+                ISpaceTypeDescriptor typeDescriptor = typeBuilder.Create();
+
+                // Register type:
+                proxy.TypeManager.RegisterTypeDescriptor(typeDescriptor);
+            }
+        }
+    }
+}
